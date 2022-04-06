ee4b663781e23af3bc2abfff5d6e36456ef8d6d6
Szaszuge <78954822+Szaszuge@users.noreply.github.com>
2022-04-06 11:35:36 +0200

    másolás

---
+++ tkinter_menu.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+abl1=Tk()
+
+def ujablak():
+    abl2 = Toplevel(abl1)
+    uz2 = Message(abl2, text="Készítette: Gipsz Jakab\nPiripócs\n2009.06.04", width=300)
+    gomb2 = Button(abl2, text="Kilép", command=abl2.destroy)
+    uz2.pack()
+    gomb2.pack()
+    abl2.mainloop()
+
+#widgetek
+szoveg1 = Label(abl1, text="Kattints a gombra!")
+gomb1 = Button(abl1, text="Névjegy",command=ujablak)
+
+#laptördelés a pack segítségével:
+szoveg1.pack()
+gomb1.pack()
+
+#indítás:
+abl1.mainloop()
